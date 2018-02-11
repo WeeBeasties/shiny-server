ba42474a540bc9e5cc061f85e72a9457b00517d3
Clifton Franklund <franklc@ferris.edu>
2018-02-10 22:25:29 -0500

    Update app.R

--- Dashboard/app.R
+++ Dashboard/app.R
@@ -55,15 +55,14 @@ ui <- dashboardPage(
 		),
 			div(id="disqus_thread",
 			    HTML(
-			    	"<script>
-			    	(function() {
-				var d = document, s = d.createElement('script');
-			    	s.src = 'https://dashboard-3.disqus.com/embed.js';
-			    	s.setAttribute('data-timestamp', +new Date());
-				(d.head || d.body).appendChild(s);
-			    	})();
-			    	</script>
-			    	<noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript' rel='nofollow'>comments powered by Disqus.</a></noscript>"
+			    	"(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://dashboard-3.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>"
 			    )
 			),
 		div(HTML('<script id="dsq-count-scr" src="//dashboard-3.disqus.com/count.js" async></script>'))
